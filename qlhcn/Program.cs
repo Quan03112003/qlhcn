bec0c5a2aca3436e7be9b8f233430af36c49b05b
quan0 <quan0@QUAN03112003>
2025-03-28 14:39:12 +0700

    Add project files.

---
+++ qlhcn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        Console.Write("Enter width of the rectangle: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter height of the rectangle: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(width, height);
+
+      
+        rectangle.Display();
+    }
+}
---
+++ qlhcn/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Rectangle
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double GetArea()
+    {
+        return Width * Height;
+    }
+
+    public double GetPerimeter()
+    {
+        return 2 * (Width + Height);
+    }
+
+ 
+    public void Display()
+    {
+        Console.WriteLine("Rectangle Information:");
+        Console.WriteLine($"Width: {Width}");
+        Console.WriteLine($"Height: {Height}");
+        Console.WriteLine($"Area: {GetArea()}");
+        Console.WriteLine($"Perimeter: {GetPerimeter()}");
+    }
+}
